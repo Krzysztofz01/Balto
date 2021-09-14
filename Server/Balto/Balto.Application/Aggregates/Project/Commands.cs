894ca422558a39b7f93d2445caba7239d2f71f17
Krzysztofz01 <krzysztof.zon2001@gmail.com>
2021-09-14 13:24:07 +0200

    Project cqrs related models and dto

---
+++ Server/Balto/Balto.Application/Aggregates/Project/Commands.cs
@@ -0,0 +1,200 @@
+﻿using Balto.Domain.Aggregates.Project.Card;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Balto.Application.Aggregates.Project
+{
+    public static class Commands
+    {
+        public static class V1
+        {
+            public class ProjectAdd
+            {
+                [Required]
+                public string Title { get; set; }
+            }
+
+            public class ProjectUpdate
+            {
+                [Required]
+                public Guid Id { get; set; }
+
+                [Required]
+                public string Title { get; set; }
+            }
+
+            public class ProjectDelete
+            {
+                [Required]
+                public Guid Id { get; set; }
+            }
+
+            public class ProjectAddContributor
+            {
+                [Required]
+                public Guid Id { get; set; }
+
+                [Required]
+                public Guid UserId { get; set; }
+            }
+
+            public class ProjectDeleteContributor
+            {
+                [Required]
+                public Guid Id { get; set; }
+
+                [Required]
+                public Guid UserId { get; set; }
+            }
+
+            public class ProjectLeave
+            {
+                [Required]
+                public Guid Id { get; set; }
+            }
+
+            public class ProjectChangeTicketStatus
+            {
+                [Required]
+                public Guid Id { get; set; }
+            }
+
+            public class ProjectAddTicket
+            {
+                [Required]
+                public string Token { get; set; }
+
+                [Required]
+                public string Title { get; set; }
+
+                [Required]
+                public string Content { get; set; }
+            }
+
+            public class ProjectAddTable
+            {
+                [Required]
+                public Guid Id { get; set; }
+
+                [Required]
+                public string Title { get; set; }
+            }
+
+            public class ProjectUpdateTable
+            {
+                [Required]
+                public Guid Id { get; set; }
+
+                [Required]
+                public Guid TableId { get; set; }
+
+                [Required]
+                public string Title { get; set;}
+
+                [Required]
+                public string Color { get; set; }
+            }
+
+            public class ProjectDeleteTable
+            {
+                [Required]
+                public Guid Id { get; set; }
+
+                [Required]
+                public Guid TableId { get; set; }
+            }
+
+            public class ProjectAddCard
+            {
+                [Required]
+                public Guid Id { get; set; }
+
+                [Required]
+                public Guid TableId { get; set; }
+
+                [Required]
+                public string Title { get; set; }
+            }
+
+            public class ProjectUpdateCard
+            {
+                [Required]
+                public Guid Id { get; set; }
+
+                [Required]
+                public Guid CardId { get; set; }
+
+                [Required]
+                public string Title { get; set; }
+
+                [Required]
+                public string Content { get; set; }
+
+                [Required]
+                public string Color { get; set; }
+
+                [Required]
+                public DateTime StartingDate { get; set; }
+
+                [Required]
+                public bool Notify { get; set; }
+
+                public DateTime? EndingDate { get; set; }
+
+                public Guid? AssignedUserId { get; set; }
+
+                [Required]
+                public CardPriorityType Priority { get; set; }
+            }
+
+            public class ProjectDeleteCard
+            {
+                [Required]
+                public Guid Id { get; set; }
+
+                [Required]
+                public Guid CardId { get; set; }
+            }
+
+            public class ProjectChangeCardStatus
+            {
+                [Required]
+                public Guid Id { get; set; }
+
+                [Required]
+                public Guid CardId { get; set; }
+            }
+
+            public class ProjectChangeCardsOrdinalNumber
+            {
+                [Required]
+                public Guid Id { get; set; }
+
+                [Required]
+                public IDictionary<Guid, int> CardOrderMap { get; set; }
+            }
+
+            public class ProjectAddComment
+            {
+                [Required]
+                public Guid Id { get; set; }
+
+                [Required]
+                public Guid CardId { get; set; }
+
+                [Required]
+                public string Content { get; set; }
+            }
+
+            public class ProjectDeleteComment
+            {
+                [Required]
+                public Guid Id { get; set; }
+
+                [Required]
+                public Guid CardId { get; set; }
+            }
+        }
+    }
+}
---
+++ Server/Balto/Balto.Application/Aggregates/Project/ProjectService.cs
@@ -0,0 +1,35 @@
+﻿using Balto.Domain.Aggregates.Project;
+using Balto.Domain.Common;
+using Balto.Infrastructure.Abstraction;
+using System;
+using System.Threading.Tasks;
+
+namespace Balto.Application.Aggregates.Project
+{
+    public class ProjectService : IApplicationService
+    {
+        private readonly IProjectRepository _projectRepository;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IRequestAuthorizationHandler _requestAuthorizationHandler;
+
+        public ProjectService(
+            IProjectRepository projectRepository,
+            IUnitOfWork unitOfWork,
+            IRequestAuthorizationHandler requestAuthorizationHandler)
+        {
+            _projectRepository = projectRepository ??
+                throw new ArgumentNullException(nameof(projectRepository));
+
+            _unitOfWork = unitOfWork ??
+                throw new ArgumentNullException(nameof(unitOfWork));
+
+            _requestAuthorizationHandler = requestAuthorizationHandler ??
+                throw new ArgumentNullException(nameof(requestAuthorizationHandler));
+        }
+
+        public Task Handle(object command)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
