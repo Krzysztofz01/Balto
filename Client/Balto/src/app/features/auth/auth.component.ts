1070012b69fa83097431ae217d5ae997817c1199
Krzysztofz01 <krzysztof.zon2001@gmail.com>
2021-09-22 13:18:57 +0200

    Login and registration page and global styles

--- Client/Balto/src/app/features/auth/auth.component.ts
+++ Client/Balto/src/app/features/auth/auth.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { AuthService } from 'src/app/authentication/services/auth.service';
+import { RegistrationModalComponent } from './registration-modal/registration-modal.component';
 
 @Component({
   selector: 'app-auth',
@@ -6,10 +11,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth.component.css']
 })
 export class AuthComponent implements OnInit {
+  public loginForm: FormGroup;
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router, private modalService: NgbModal) { }
 
   ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [ Validators.required, Validators.email ]),
+      password: new FormControl('', [ Validators.required ])
+    });
   }
 
+  //Get login credentials and post to auth endpoint via authservice
+  public loginSubmit(): void {
+    if(this.loginForm.valid) {
+      this.authService.login(
+        this.loginForm.get('email').value,
+        this.loginForm.get('password').value
+      ).subscribe(() => {
+        this.router.navigate(['']);
+      },
+      (error) => {
+        //TODO: Toast service server error
+      });  
+    } else {
+      //TODO: Toast service validation error
+    }
+
+    this.loginForm.reset();
+  }
+
+  //Launch registration modal and register if data is valid
+  public register(): void {
+    this.modalService.open(RegistrationModalComponent, { modalDialogClass: 'modal-lg'}).result.then((credentials) => {
+      this.authService.register(credentials.name, credentials.email, credentials.password, credentials.passwordRepeat).subscribe(() => {
+        //TODO: Toast service show register info
+      },
+      (error) => {
+        //TODO: Toast service show server error
+      });
+    }, () => {});
+  }
 }
--- Client/Balto/src/app/features/auth/auth.module.ts
+++ Client/Balto/src/app/features/auth/auth.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthComponent } from './auth.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RegistrationModalComponent } from './registration-modal/registration-modal.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 
 @NgModule({
   declarations: [
-    AuthComponent
+    AuthComponent,
+    RegistrationModalComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    NgbModule
   ]
 })
 export class AuthModule { }
