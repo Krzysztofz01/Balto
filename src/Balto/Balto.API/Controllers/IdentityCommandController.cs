ff115b2df3c7dc31930e592c4d31ab20a522ead9
Krzysztofz01 <krzysztof.zon2001@gmail.com>
2021-11-11 12:58:21 +0100

    Authentication and identity related controllers

---
+++ src/Balto/Balto.API/Controllers/IdentityCommandController.cs
@@ -0,0 +1,45 @@
+﻿using Balto.API.Utility;
+using Balto.Application.Abstraction;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+using static Balto.Application.Identities.Commands;
+
+namespace Balto.API.Controllers
+{
+    [ApiController]
+    [Route("api/v{version:apiVersion}/identity")]
+    [ApiVersion("1.0")]
+    [Authorize(Roles = "Admin")]
+    public class IdentityCommandController : ControllerBase
+    {
+        private readonly IIdentityService _identityService;
+
+        public IdentityCommandController(IIdentityService identityService)
+        {
+            _identityService = identityService ??
+                throw new ArgumentNullException(nameof(identityService));
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(V1.Delete command) =>
+            await RequestHandler.Command(command, _identityService.Handle);
+
+        [HttpPut]
+        public async Task<IActionResult> Update(V1.Update command) =>
+            await RequestHandler.Command(command, _identityService.Handle);
+
+        [HttpPut("activation")]
+        public async Task<IActionResult> Activation(V1.Activation command) =>
+            await RequestHandler.Command(command, _identityService.Handle);
+
+        [HttpPut("role")]
+        public async Task<IActionResult> RoleChange(V1.RoleChange command) =>
+            await RequestHandler.Command(command, _identityService.Handle);
+
+        [HttpPut("team")]
+        public async Task<IActionResult> TeamChange(V1.TeamChange command) =>
+            await RequestHandler.Command(command, _identityService.Handle);
+    }
+}
---
+++ src/Balto/Balto.API/Controllers/IdentityQueryController.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using Balto.API.Utility;
+using Balto.Application.Identities;
+using Balto.Domain.Identities;
+using Balto.Infrastructure.Core.Abstraction;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using static Balto.Application.Identities.Dto;
+
+namespace Balto.API.Controllers
+{
+    [ApiController]
+    [Route("api/v{version:apiVersion}/identity")]
+    [ApiVersion("1.0")]
+    public class IdentityQueryController : ControllerBase
+    {
+        private readonly IDataAccess _dataAccess;
+        private readonly IMapper _mapper;
+
+        public IdentityQueryController(IDataAccess dataAccess, IMapper mapper)
+        {
+            _dataAccess = dataAccess ??
+                throw new ArgumentNullException(nameof(dataAccess));
+
+            _mapper = mapper ??
+                throw new ArgumentNullException(nameof(mapper));
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "Admin, Support")]
+        public async Task<IActionResult> GetAll() =>
+            await RequestHandler.MapQuery<IEnumerable<Identity>, IEnumerable<IdentitySimple>>(_dataAccess.Identities.GetAllIdentities(), _mapper);
+
+        [HttpGet("{identityId}")]
+        [Authorize(Roles = "Admin, Support")]
+        public async Task<IActionResult> GetById(Guid identityId) =>
+            await RequestHandler.MapQuery<Identity, IdentityDetails>(_dataAccess.Identities.GetIdentityById(identityId), _mapper);
+
+        [HttpGet("team/{teamId}")]
+        [Authorize]
+        public async Task<IActionResult> GeAllInTeam(Guid teamId) =>
+            await RequestHandler.MapQuery<IEnumerable<Identity>, IEnumerable<IdentitySimple>>(_dataAccess.Identities.GetAllIdentitiesInTeam(teamId), _mapper);
+    }
+}
