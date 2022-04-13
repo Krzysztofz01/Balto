c4d175a493ffb6d4d0648cb1b645df90ef5ba1fd
Krzysztofz01 <krzysztof.zon2001@gmail.com>
2022-04-13 18:20:15 +0200

    Tags support for project aggregate

---
+++ src/Balto/Balto.Domain/Projects/ProjectTags/ProjectTagId.cs
@@ -0,0 +1,17 @@
+﻿using Balto.Domain.Shared;
+using System;
+
+namespace Balto.Domain.Projects.ProjectTags
+{
+    public class ProjectTagId : Identifier
+    {
+        private ProjectTagId() { }
+        private ProjectTagId(Guid value) : base(value) { }
+
+        public static implicit operator string(ProjectTagId contributorId) => contributorId.Value.ToString();
+        public static implicit operator Guid(ProjectTagId contributorId) => contributorId.Value;
+
+        public static ProjectTagId FromGuid(Guid guid) => new ProjectTagId(guid);
+        public static ProjectTagId FromString(string guid) => new ProjectTagId(Guid.Parse(guid));
+    }
+}
