7499dae995a9fbbdab13759204fa21b2719a859f
Krzysztofz01 <krzysztof.zon2001@gmail.com>
2022-04-13 17:56:25 +0200

    Tag aggregate implemented

---
+++ src/Balto/Balto.Domain/Tags/TagTitle.cs
@@ -0,0 +1,27 @@
+﻿using Balto.Domain.Core.Exceptions;
+using Balto.Domain.Core.Extensions;
+using Balto.Domain.Core.Model;
+
+namespace Balto.Domain.Tags
+{
+    public class TagTitle : ValueObject<TagTitle>
+    {
+        private const int _maxLength = 100;
+
+        public string Value { get; private set; }
+
+        private TagTitle() { }
+        public TagTitle(string value)
+        {
+            if (value.IsEmpty() || !value.IsLengthLess(_maxLength))
+                throw new ValueObjectValidationException("Invalid project tag title length.");
+
+            Value = value;
+        }
+
+
+        public static implicit operator string(TagTitle title) => title.Value;
+
+        public static TagTitle FromString(string title) => new TagTitle(title);
+    }
+}
