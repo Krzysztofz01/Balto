bd8c4230d442f23c0e29a00d8333b5a34998052e
Krzysztofz01 <krzysztof.zon2001@gmail.com>
2022-04-14 18:36:14 +0200

    Tags implemented for idataaccess contract

--- src/Balto/Balto.Infrastructure.Core/Abstraction/IDataAccess.cs
+++ src/Balto/Balto.Infrastructure.Core/Abstraction/IDataAccess.cs
@@ -1,6 +1,8 @@
 ﻿using Balto.Domain.Goals;
 using Balto.Domain.Identities;
 using Balto.Domain.Projects;
+using Balto.Domain.Tags;
+using System;
 using System.Linq;
 
 namespace Balto.Infrastructure.Core.Abstraction
@@ -10,9 +12,15 @@ namespace Balto.Infrastructure.Core.Abstraction
         IQueryable<Identity> Identities { get; }
         IQueryable<Project> Projects { get; }
         IQueryable<Goal> Goals { get; }
+        IQueryable<Tag> Tags { get; }
 
+        [Obsolete]
         IQueryable<Identity> IdentitiesTracked { get; }
+        [Obsolete]
         IQueryable<Project> ProjectsTracked { get; }
+        [Obsolete]
         IQueryable<Goal> GoalsTracked { get; }
+        [Obsolete]
+        IQueryable<Tag> TagsTracked { get; }
     }
 }
