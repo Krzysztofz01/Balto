87ed70d8947ebaff3ddc52d071376666eb4fb246
Krzysztofz01 <krzysztof.zon2001@gmail.com>
2022-04-14 18:38:53 +0200

    Api layer tag aggregate implementation

---
+++ src/Balto/Balto.API/Controllers/TagCommandController.cs
@@ -0,0 +1,30 @@
+﻿using Balto.API.Controllers.Base;
+using Balto.Application.Abstraction;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+using static Balto.Application.Tags.Commands;
+
+namespace Balto.API.Controllers
+{
+    [Route("api/v{version:apiVersion}/tag")]
+    [ApiVersion("1.0")]
+    public class TagCommandController : CommandController
+    {
+        private readonly ITagService _tagService;
+
+        public TagCommandController(ITagService tagService) : base()
+        {
+            _tagService = tagService ??
+                throw new ArgumentNullException(nameof(tagService));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(V1.Create command) =>
+            await Command(command, _tagService.Handle);
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(V1.Delete command) =>
+            await Command(command, _tagService.Handle);
+    }
+}
