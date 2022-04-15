fa2e158208e81f9e437c34651de9c89f6ba9bf22
Krzysztofz01 <krzysztof.zon2001@gmail.com>
2022-04-15 15:03:21 +0200

    Note related entities with value objects

---
+++ src/Balto/Balto.Domain/Notes/NoteContributors/NoteContributor.cs
@@ -0,0 +1,22 @@
+﻿using Balto.Domain.Core.Events;
+using Balto.Domain.Core.Model;
+using System;
+
+namespace Balto.Domain.Notes.NoteContributors
+{
+    public class NoteContributor : Entity
+    {
+        public NoteContributorIdentityId IdentityId { get; private set; }
+        public NoteContributorAccessRole AccessRole { get; private set; }
+
+        protected override void Handle(IEventBase @event)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void Validate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Balto/Balto.Domain/Notes/NoteTags/NoteTag.cs
@@ -0,0 +1,21 @@
+﻿using Balto.Domain.Core.Events;
+using Balto.Domain.Core.Model;
+using System;
+
+namespace Balto.Domain.Notes.NoteTags
+{
+    public class NoteTag : Entity
+    {
+        public NoteTagId TagId { get; private set; }
+
+        protected override void Handle(IEventBase @event)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void Validate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
