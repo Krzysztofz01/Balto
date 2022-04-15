41e97d33f0d757f7b4efa85ec8540a568ccfcbc9
Krzysztofz01 <krzysztof.zon2001@gmail.com>
2022-04-15 15:03:52 +0200

    Note aggregate boilerplate

---
+++ src/Balto/Balto.Domain/Notes/Note.cs
@@ -0,0 +1,38 @@
+﻿using Balto.Domain.Core.Events;
+using Balto.Domain.Core.Extensions;
+using Balto.Domain.Core.Model;
+using Balto.Domain.Notes.NoteContributors;
+using Balto.Domain.Notes.NoteTags;
+using System;
+using System.Collections.Generic;
+
+namespace Balto.Domain.Notes
+{
+    public class Note : AggregateRoot
+    {
+        public NoteTitle Title { get; private set; }
+        public NoteContent Content { get; private set; }
+
+        private readonly List<NoteContributor> _contributors;
+        public IReadOnlyCollection<NoteContributor> Contributors => _contributors.SkipDeleted().AsReadOnly();
+
+        private readonly List<NoteTag> _tags;
+        public IReadOnlyCollection<NoteTag> Tags => _tags.SkipDeleted().AsReadOnly();
+
+        protected override void Handle(IEventBase @event)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void Validate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Note()
+        {
+            _contributors = new List<NoteContributor>();
+            _tags = new List<NoteTag>();
+        }
+    }
+}
