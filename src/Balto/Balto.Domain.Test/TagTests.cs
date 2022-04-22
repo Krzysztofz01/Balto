34e9e3039511ac89fb74c420b27c79cff42e0bfd
Krzysztofz01 <krzysztof.zon2001@gmail.com>
2022-04-22 16:23:53 +0200

    Tag tests implementation

---
+++ src/Balto/Balto.Domain.Test/TagTests.cs
@@ -0,0 +1,50 @@
+﻿using Balto.Domain.Tags;
+using Xunit;
+
+namespace Balto.Domain.Test
+{
+    public class TagTests
+    {
+        [Fact]
+        public void TagShouldCreate()
+        {
+            Tag.Factory.Create(new Events.V1.TagCreated
+            {
+                Color = "#123ABC",
+                Title = "Tag title"
+            });
+        }
+
+        // Shared color value object is not implemented
+        /*[Fact]
+        public void TagShouldThrowForInvalidColor()
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                Tag.Factory.Create(new Events.V1.TagCreated
+                {
+                    Color = "@QQQZZZ",
+                    Title = "Tag title"
+                });
+            });
+        }
+        */
+
+        [Fact]
+        public void TagShouldDelete()
+        {
+            var tag = Tag.Factory.Create(new Events.V1.TagCreated
+            {
+                Color = "#123ABC",
+                Title = "Tag title"
+            });
+
+            tag.Apply(new Events.V1.TagDeleted
+            {
+                Id = tag.Id
+            });
+
+            Assert.NotNull(tag.DeletedAt);
+        }
+    }
+}
