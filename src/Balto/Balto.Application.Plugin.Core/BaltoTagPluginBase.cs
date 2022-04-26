2356241eeaf5df4a83bd7765128317fe5975d43a
Krzysztofz01 <krzysztof.zon2001@gmail.com>
2022-04-26 10:23:46 +0200

    Plugin base for missing aggregates

---
+++ src/Balto/Balto.Application.Plugin.Core/BaltoTagPluginBase.cs
@@ -0,0 +1,29 @@
+﻿using Balto.Domain.Tags;
+using Balto.Infrastructure.Core.Abstraction;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace Balto.Application.Plugin.Core
+{
+    public abstract class BaltoTagPluginBase<TPlugin> : BaltoPluginBase<TPlugin> where TPlugin : BaltoPluginBase<TPlugin>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly ILogger<TPlugin> _logger;
+
+        protected ITagRepository TagRepository => _unitOfWork.TagRepository;
+        protected ILogger<TPlugin> Logger => _logger;
+
+        protected async Task CommitChanges() => await _unitOfWork.Commit();
+
+        private BaltoTagPluginBase() { }
+        public BaltoTagPluginBase(IUnitOfWork unitOfWork, ILogger<TPlugin> logger)
+        {
+            _unitOfWork = unitOfWork ??
+                throw new ArgumentNullException(nameof(unitOfWork));
+
+            _logger = logger ??
+                throw new ArgumentNullException(nameof(logger));
+        }
+    }
+}
