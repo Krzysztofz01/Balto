c5c7d64b886fa8df3ec53a4648812bbb66ade6a4
Krzysztofz01 <krzysztof.zon2001@gmail.com>
2022-07-14 13:06:43 +0200

    Tag validation check

--- src/Balto/Balto.Application/Tags/Commands.cs
+++ src/Balto/Balto.Application/Tags/Commands.cs
@@ -1,7 +1,6 @@
 ﻿using Balto.Application.Abstraction;
 using Balto.Domain.Tags;
 using System;
-using System.ComponentModel.DataAnnotations;
 
 namespace Balto.Application.Tags
 {
@@ -11,16 +10,13 @@ namespace Balto.Application.Tags
         {
             public class Create : IApplicationCommand<Tag>
             {
-                [Required]
                 public string Title { get; set; }
 
-                [Required]
                 public string Color { get; set; }
             }
 
             public class Delete : IApplicationCommand<Tag>
             {
-                [Required]
                 public Guid Id { get; set; }
             }
         }
