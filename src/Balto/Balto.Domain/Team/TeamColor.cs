186044831c9d47357443acd5679282c594d1c9be
Krzysztofz01 <krzysztof.zon2001@gmail.com>
2022-07-15 12:25:29 +0200

    Team value object and event implementation

---
+++ src/Balto/Balto.Domain/Team/TeamColor.cs
@@ -0,0 +1,18 @@
+﻿using Balto.Domain.Shared;
+
+namespace Balto.Domain.Team
+{
+    public class TeamColor : Color
+    {
+        private TeamColor() : base() { }
+        private TeamColor(string value) : base(value) { }
+        private TeamColor(System.Drawing.Color color) : base(color) { }
+
+        public static implicit operator string(TeamColor color) => color.Value;
+        public static implicit operator System.Drawing.Color(TeamColor color) => color.ToColor();
+
+        public static TeamColor Default => new TeamColor(_defaultValue);
+        public static TeamColor FromString(string color) => new TeamColor(color);
+        public static TeamColor FromColor(System.Drawing.Color color) => new TeamColor(color);
+    }
+}
---
+++ src/Balto/Balto.Domain/Team/TeamMembers/TeamMemberIdentityId.cs
@@ -0,0 +1,17 @@
+﻿using Balto.Domain.Shared;
+using System;
+
+namespace Balto.Domain.Team.TeamMembers
+{
+    public class TeamMemberIdentityId : Identifier
+    {
+        private TeamMemberIdentityId() { }
+        private TeamMemberIdentityId(Guid value) : base(value) { }
+
+        public static implicit operator string(TeamMemberIdentityId contributorId) => contributorId.Value.ToString();
+        public static implicit operator Guid(TeamMemberIdentityId contributorId) => contributorId.Value;
+
+        public static TeamMemberIdentityId FromGuid(Guid guid) => new TeamMemberIdentityId(guid);
+        public static TeamMemberIdentityId FromString(string guid) => new TeamMemberIdentityId(Guid.Parse(guid));
+    }
+}
