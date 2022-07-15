3047125d2086e6ce37eb000ef307b0ed5c8bc13f
Krzysztofz01 <krzysztof.zon2001@gmail.com>
2022-07-15 12:25:59 +0200

    Aggregate and helper entity implementation

---
+++ src/Balto/Balto.Domain/Team/TeamMembers/TeamMember.cs
@@ -0,0 +1,49 @@
+﻿using Balto.Domain.Core.Events;
+using Balto.Domain.Core.Exceptions;
+using Balto.Domain.Core.Model;
+using System;
+using static Balto.Domain.Team.Events;
+
+namespace Balto.Domain.Team.TeamMembers
+{
+    public class TeamMember : Entity
+    {
+        public TeamMemberIdentityId IdentityId { get; private set; }
+
+        protected override void Handle(IEventBase @event)
+        {
+            switch (@event)
+            {
+                case V1.TeamMemberDeleted e: When(e); break;
+
+                default: throw new BusinessLogicException("This entity can not handle this type of event.");
+            }
+        }
+
+        private void When(V1.TeamMemberDeleted _)
+        {
+            DeletedAt = DateTime.Now;
+        }
+
+        protected override void Validate()
+        {
+            bool isNull = IdentityId is null;
+
+            if (isNull)
+                throw new BusinessLogicException("The project aggregate properties can not be null.");
+        }
+
+        private TeamMember() { }
+
+        public static class Factory
+        {
+            public static TeamMember Create(V1.TeamMemberAdded @event)
+            {
+                return new TeamMember
+                {
+                    IdentityId = TeamMemberIdentityId.FromGuid(@event.IdentityId)
+                };
+            }
+        }
+    }
+}
