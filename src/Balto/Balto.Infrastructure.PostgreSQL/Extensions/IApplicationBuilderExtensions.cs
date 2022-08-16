350f72b737148e23d9d621157acf20431c863b4a
Krzysztof Zoń <krzysztof.zon2001@gmail.com>
2022-08-16 19:16:43 +0200

    Postgressql registartion

---
+++ src/Balto/Balto.Infrastructure.PostgreSQL/Extensions/IApplicationBuilderExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Balto.Infrastructure.PostgreSQL.Extensions
+{
+    public static class IApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder UsePostgreSQLInfrastructure(this IApplicationBuilder app, IServiceProvider service)
+        {
+            using var dbContext = service.GetRequiredService<BaltoDbContext>();
+
+            dbContext.Database.Migrate();
+
+            return app;
+        }
+    }
+}
---
+++ src/Balto/Balto.Infrastructure.PostgreSQL/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Balto.Infrastructure.Core.Abstraction;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Balto.Infrastructure.PostgreSQL.Extensions
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddPostgreSQLInfrastructure(this IServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<BaltoDbContext>(options =>
+                options.UseNpgsql(connectionString));
+
+            services.AddScoped<IAuthenticationDataAccessService, AuthenticationDataAccessService>();
+
+            services.AddScoped<IDataAccess, DataAccess>();
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            return services;
+        }
+    }
+}
